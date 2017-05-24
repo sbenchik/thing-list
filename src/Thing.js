2e71398d863fcc6f3a5769d51f6d6c2cd343b14d
Steve Benchik <stevebenchik@gmail.com>
2017-05-24 10:45:16 -0400

    Extract actions span into its own component and CSS file

--- src/Thing.js
+++ src/Thing.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ContentEditable from 'react-contenteditable'
 
+import Actions from './Actions'
 import './Thing.css'
 
 const Thing = ({ thing, saveThing, removeThing }) => {
@@ -14,11 +15,7 @@ const Thing = ({ thing, saveThing, removeThing }) => {
             <input type="checkbox" value="on" />
             <div className="details">
                 <ContentEditable className="name" html={thing.name} onChange = {updateName}/>
-                <span className="actions">
-                    <button className="remove" onClick={() => removeThing(thing)}>
-                        <i className="fa fa-trash-o"></i>
-                    </button>
-                </span>
+                <Actions thing={thing} removeThing={removeThing}/>               
             </div>
         </li>
     )
